466e79931c04738e854c48718910aff2237425c9
cokoyoh <charlesokoyoh@gmail.com>
2017-11-27 06:58:49 +0000

    seeding complete

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Item::class, function (Faker $faker) {
     return [
-        'name' => $faker->words(3,false),
+        'name' => $faker-> word,
         'description' => $faker->paragraph(3,true),
         'total_number' => $faker->randomNumber(3, false),
         'price' =>  $faker->randomNumber(6,false)
