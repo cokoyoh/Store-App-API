b2ecf7eda6cff4a69666d0ef5ba5801aa7c7868c
cokoyoh <charlesokoyoh@gmail.com>
2017-11-30 19:11:43 +0300

    curating orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,15 +3,34 @@
 namespace App\Http\Controllers;
 
 use App\Order;
-use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth')->except('index');
+    }
     public function index() {
-        $orders = Order::orderBy('id', 'desc')->get();
+        $orders = Order::orderBy('id', 'desc')->paginate(5)->get();
         return response(['data' => $orders], 200);
     }
     // find orders that have been made by a user
-    // find orders that have been made of a particular item
+    public function allOrdersMadeByUser() {
+        $orders = Order::with('items')->where('user_id', '=', Auth::user()->id)->get();
+        return response(['data' => $orders],200);
+    }
+    //orders made by user today
+    public function ordersMadeByUserToday() {
+        $orders = Order::with('items')->where('user_id', '=', Auth::user()->id)
+            ->where('created_at', '=', Carbon::today())->get();
+        return response(['data' => $orders],200);
+    }
 
+    //orders made today
+    public function ordersReceivedToday() {
+        $orders = Order::where('created_at', '=', Carbon::today())->paginate(5)->get();
+        return response(['data' => $orders],200);
+    }
+    // find orders that have been made of a particular item
 }
